71011446022017220b1b38a6963f41d1ac94ea92
Mostafa Rowghanian <mostafatech@gmail.com>
2020-12-15 02:45:40 +0330

    Merge feature/gitflow-control to develop (#1)
    * add project.sh file and simple workflow
    * fix pull-request title in project.sh
    * add push and feature-done to project.sh

---
+++ project.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BRANCH_DEVELOP=develop
+BRANCH_CURRENT=$(git branch --show-current)
+GIT_REMOTE_NAME=origin
+
+start_dev() {
+	echo "starting development"
+}
+
+branch_push() {
+	if [[ $1 != '' ]]; then
+		git push -u $GIT_REMOTE_NAME $1
+	else
+		git push -u $GIT_REMOTE_NAME $BRANCH_CURRENT
+	fi
+}
+
+feature_start() {
+	git flow feature start $1
+}
+
+feature_send() {
+	branch_push
+	gh pr create \
+		--head $BRANCH_CURRENT \
+		--base $BRANCH_DEVELOP \
+		--title "Merge $BRANCH_CURRENT to $BRANCH_DEVELOP" \
+		--web
+}
+
+feature_done() {
+	echo "done feature actions ..."
+}
+
+# main
+if [[ $1 == 'dev' ]]; then start_dev
+elif [[ $1 == 'push' ]]; then branch_push $2
+elif [[ $1 == 'feature-start' ]]; then feature_start $2
+elif [[ $1 == 'feature-send' ]]; then feature_send
+elif [[ $1 == 'feature-done' ]]; then feature_done
+else
+	echo "Please specify one of the actions"
+fi
